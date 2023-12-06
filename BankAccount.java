77a12a4f5471fda13e5b55e7704287ce859dd997
KKeerthana0610 <105630367+KKeerthana0610@users.noreply.github.com>
2023-12-06 15:06:59 +0530

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -1,62 +1,60 @@
-package assignment1;
+package assignment2;
+
+public class BankAccount {
+
+	private String accountHolderName;
+    private int accountNumber;
+    private int balance;
+
+    // Constructor
+    public BankAccount(String accountHolderName, int accountNumber, int balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    // Getters and setters
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    // Deposit method
+    public void deposit(int amount) {
+        balance += amount;
+        System.out.println(amount + " deposited successfully.");
+    }
+
+    // Withdraw method
+    public void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println(amount + " withdrawn successfully.");
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    }
 
 
-	public class BankAccount {
-		private String accountHolderName;
-	    private String bankName;
-	    private double accountBalance;
-
-	    public BankAccount(String accountHolderName, String bankName, double initialBalance) {
-	        this.accountHolderName = accountHolderName;
-	        this.bankName = bankName;
-	        this.accountBalance = initialBalance;
-	    }
-	    public double getBalance() {
-	        return accountBalance;
-	    }
-	    public void deposit(double amount) {
-	        if (amount > 0) {
-	            accountBalance += amount;
-	            System.out.println("Deposited $" + amount + " into " + bankName + " account of " + accountHolderName);
-	        } else {
-	            System.out.println("Invalid deposit amount");
-	        }
-	    }
-	    public void withdraw(double amount) {
-	        if (amount > 0 && amount <= accountBalance) {
-	            accountBalance -= amount;
-	            System.out.println("Withdrawn $" + amount + " from " + bankName + " account of " + accountHolderName);
-	        } else {
-	            System.out.println("Invalid withdrawal amount or insufficient funds");
-	        }
-	    }
-
-	    public static void main(String[] args) {
-	        BankAccount account1 = new BankAccount("ManojKumar", "ICICI", 1000.0);
-	        BankAccount account2 = new BankAccount("Keerthana", "HDFC", 1000.0);
-	        BankAccount account3 = new BankAccount("Harish", "SBI", 1500.0);
-	        account1.deposit(500.0);
-	        account1.withdraw(200.0);
-
-	        account2.deposit(1000.0);
-	        account2.withdraw(500.0);
-
-	        account3.deposit(300.0);
-	        account3.withdraw(2000.0);
-	        System.out.println("\nAccount Balances:");
-	        System.out.println(account1.accountHolderName + "'s " + account1.bankName + " account balance: Rs" + account1.getBalance());
-	        System.out.println(account2.accountHolderName + "'s " + account2.bankName + " account balance: Rs" + account2.getBalance());
-	        System.out.println(account3.accountHolderName + "'s " + account3.bankName + " account balance: RS" + account3.getBalance());
-	    }
-	    
-	    
-	
+	}
 
 
-}
-	
-
-
-
-	
-	
