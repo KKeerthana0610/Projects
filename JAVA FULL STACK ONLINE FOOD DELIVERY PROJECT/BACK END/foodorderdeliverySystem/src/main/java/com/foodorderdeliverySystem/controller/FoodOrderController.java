e94f27612d1acd3d3fa0f1388013fac3f170cf9f
KKeerthana0610 <105630367+KKeerthana0610@users.noreply.github.com>
2024-01-17 22:12:35 +0530

    Update FoodOrderController.java

--- JAVA FULL STACK ONLINE FOOD DELIVERY PROJECT/BACK END/foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
+++ JAVA FULL STACK ONLINE FOOD DELIVERY PROJECT/BACK END/foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
@@ -14,6 +14,7 @@ import com.foodorderdeliverySystem.entity.FoodOrder;
 import com.foodorderdeliverySystem.service.FoodOrderService;
 
 @RestController
+	@CrossOrigin(origins="http://localhost:4200")
 public class FoodOrderController {
 	
 	 @Autowired
