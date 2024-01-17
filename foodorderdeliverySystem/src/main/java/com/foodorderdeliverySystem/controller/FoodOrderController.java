87c4c100ad7fdacfceb72bd6bfaaa04546350b4a
KKeerthana0610 <105630367+KKeerthana0610@users.noreply.github.com>
2024-01-17 22:14:30 +0530

    Update FoodOrderController.java

--- foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
+++ foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
@@ -1,5 +1,4 @@
 package com.foodorderdeliverySystem.controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import com.foodorderdeliverySystem.entity.FoodOrder;
 import com.foodorderdeliverySystem.service.FoodOrderService;
 
 @RestController
-	@CrossOrigin(origins="http://localhost:4200")
 public class FoodOrderController {
 	
 	 @Autowired
