fad26dfbe379a2a0c6f5d1000eb5b17ebc71a3bc
KKeerthana0610 <105630367+KKeerthana0610@users.noreply.github.com>
2024-01-17 22:10:29 +0530

    Update FoodOrderController.java

--- JAVA FULL STACK ONLINE FOOD DELIVERY PROJECT/BACK END/foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
+++ JAVA FULL STACK ONLINE FOOD DELIVERY PROJECT/BACK END/foodorderdeliverySystem/src/main/java/com/foodorderdeliverySystem/controller/FoodOrderController.java
@@ -1,5 +1,5 @@
 package com.foodorderdeliverySystem.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
